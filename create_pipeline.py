b5de786331a8002357395fcea793220c0e5ee100
eikku <eijaleena.koponen@gmail.com>
2021-10-27 14:39:06 +0300

    Update inputs

--- create_pipeline.py
+++ create_pipeline.py
@@ -14,9 +14,9 @@ def main(config) -> Pipeline:
     # Configure the pipeline, i.e. define the edges.
     fetch.output("*").to(process.input("attacksmini"))
     process.output("*").to(pepare_text.input("attacksminiprocessed"))
-    pepare_text.output("*").to(fine_tune.input("train"))
-    pepare_text.output("*").to(fine_tune.input("val"))
-    pepare_text.output("*").to(fine_tune.input("test"))
-    pepare_text.output("*").to(fine_tune.input("my_dict"))
+    pepare_text.output("train.csv").to(fine_tune.input("train"))
+    pepare_text.output("val.csv").to(fine_tune.input("val"))
+    pepare_text.output("test.csv").to(fine_tune.input("test"))
+    pepare_text.output("my_dict.csv").to(fine_tune.input("my_dict"))
 
     return pipe
