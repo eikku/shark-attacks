49c373ad20f84c543fd94d8a3b17113b18737b4a
eikku <eijaleena.koponen@gmail.com>
2021-12-17 16:08:28 +0200

    Add wandb project and login

--- fecth_data.py
+++ fecth_data.py
@@ -3,13 +3,16 @@ import pandas as pd
 import valohai
 import wandb
 
+wandb.login()
+wandb.init(project="huggin-project", entity="eikku")
+
 inputs = {"attacks": "datum://017c9ddc-ce57-672e-9358-50a2ffb9115e"}
 valohai.prepare(step="fetch_data", default_inputs=inputs)
 
 file_path = valohai.inputs('attacks').path()
 attacks = pd.read_csv(file_path,encoding='cp1252')
 
-wandb.log_artifact(file_path, name='data_artifact', type='my_dataset') 
+wandb.log_artifact(file_path, name='data_artifact', type='my_dataset')
 
 attacks.columns = attacks.columns.str.strip()
 
